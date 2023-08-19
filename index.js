da41b88775a0949926263c32ed35ec4e08b31d45
itsalivenger <alihoussa16@gmail.com>
2023-08-19 20:53:52 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+let express = require("express");
+let cors = require("cors");
+let app = express();
+const PORT = 3001;
+
+app.use(express.json());
+app.use(express.text());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(
+  cors({
+    origin: ["http://localhost:3000", "*://localhost:*/*"],
+  })
+);
+
+app.post("/login", (req, res, next) => {
+  console.log(req.body);
+});
+
+app.post("/signup", (req, res, next) => {
+  console.log(req.body);
+});
+
+app.get("/login", (req, res, next) => {
+  res.send({ name: "hey" });
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT} for requests`);
+});
