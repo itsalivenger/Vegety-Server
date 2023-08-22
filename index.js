3395bddc5f5cd9309ef14b1f6f744780d233c275
itsalivenger <alihoussa16@gmail.com>
2023-08-22 23:14:13 +0100

    updated cors

--- index.js
+++ index.js
@@ -1,63 +1,63 @@
 let express = require("express");
-// let cors = require("cors");
+let cors = require("cors");
 let db = require("./mongodbConnection");
-// let prodsRouter = require("./routes/productsRoute");
-// let newsLetterRoouter = require("./routes/newsletterRoute");
-// let loginRouter = require("./routes/loginRoute");
-// let signupRouter = require("./routes/signupRoute");
-// let searchRouter = require("./routes/searchRoute");
-// let cartelRouter = require("./routes/cartelRoute");
-// let adminRouter = require("./routes/adminRoute");
+let prodsRouter = require("./routes/productsRoute");
+let newsLetterRoouter = require("./routes/newsletterRoute");
+let loginRouter = require("./routes/loginRoute");
+let signupRouter = require("./routes/signupRoute");
+let searchRouter = require("./routes/searchRoute");
+let cartelRouter = require("./routes/cartelRoute");
+let adminRouter = require("./routes/adminRoute");
 let app = express();
 require("dotenv").config();
 const PORT = process.env.PORT || 5050;
 
-// app.use((req, res, next) => {
-//   req.db = db;
-//   next();
-// });
-// app.use(express.json());
-// app.use(express.text());
-// app.use(
-//   express.urlencoded({
-//     extended: true,
-//   })
-// );
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:3000",
-//       "*://localhost:*",
-//       "https://vegety-crem.vercel.app",
-//     ],
-//     credentials: true
-//   })
-// );
+app.use((req, res, next) => {
+  req.db = db;
+  next();
+});
+app.use(express.json());
+app.use(express.text());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",
+      "*://localhost:*",
+      "https://vegety-crem.vercel.app",
+    ],
+    credentials: true
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("dedrno");
 });
 
-// // Prducts Express Roouter
-// app.use("/products", prodsRouter);
+// Prducts Express Roouter
+app.use("/products", prodsRouter);
 
-// // newsLetter Express Router
-// app.use("/newsletter", newsLetterRoouter);
+// newsLetter Express Router
+app.use("/newsletter", newsLetterRoouter);
 
-// // login Express Router
-// app.use("/login", loginRouter);
+// login Express Router
+app.use("/login", loginRouter);
 
-// // signup Express Router
-// app.use("/signup", signupRouter);
+// signup Express Router
+app.use("/signup", signupRouter);
 
-// // search Express Router
-// app.use("/search", searchRouter);
+// search Express Router
+app.use("/search", searchRouter);
 
-// // cartel Express Router
-// app.use("/Cartel", cartelRouter);
+// cartel Express Router
+app.use("/Cartel", cartelRouter);
 
-// // Admin Express Router
-// app.use("/admin", adminRouter);
+// Admin Express Router
+app.use("/admin", adminRouter);
 
 app.listen(PORT, () => {
   console.log(`listening on port ${PORT} for requests`);
