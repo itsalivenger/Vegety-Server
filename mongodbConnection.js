02140ad4348f7065dd22a5bde44c568e9429f55f
itsalivenger <alihoussa16@gmail.com>
2023-08-22 23:09:06 +0100

    updated cors

--- mongodbConnection.js
+++ mongodbConnection.js
@@ -1,5 +1,4 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
-let app = require("express")();
 require("dotenv").config();
 // Create a MongoClient with a MongoClient Options object to set the Stable API version
 let dbName = "Vegety";
