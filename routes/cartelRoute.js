e21c50c25b9d5b45484ad00e219f53f4de575f52
itsalivenger <alihoussa16@gmail.com>
2023-09-05 03:07:19 +0100

    sme routing in cart

--- routes/cartelRoute.js
+++ routes/cartelRoute.js
@@ -1,8 +1,34 @@
 let router = require("express").Router();
+let { ObjectId } = require("mongodb");
 
-router.post("/", (req, res) => {
-  console.log("cartel");
-  res.send({ txt: "cartel" });
+
+function transformIdsObject(arr) {
+  let res = arr.map(e => {
+    return new ObjectId(e._id);
+  });
+  return res;
+}
+
+router.post("/", async (req, res) => {
+  let cartel = req.body;
+  let collection = req.db.collection("Products");
+  cartel = transformIdsObject(cartel);
+
+  let prods = await collection.find({_id: { $in: cartel }}).toArray();
+  
+  res.send(prods);
 });
+
+router.delete("/", async (req, res)=>{
+  let user = req.body;
+  let _id = new ObjectId(user._id);
+  let collection = req.db.collection("Users");
+  let updatedCart = await collection.updateOne({_id}, {$set: {cart: user.cart}});
+  if(updatedCart){
+    res.status(200).send(updatedCart);
+  }else{
+    res.status(403).send(updatedCart);
+  }
+})
 
 module.exports = router;
--- routes/productPreviewRoute.js
+++ routes/productPreviewRoute.js
@@ -8,4 +8,16 @@ router.get("/:itemId", async (req, res)=>{
     res.send(prod);
 })
 
+router.put("/", async (req, res)=>{
+    let user = req.body;
+    let _id = new ObjectId(user._id);
+    let collection = req.db.collection("Users");
+    let updatedUser = await collection.updateOne({_id}, {$set: {cart: user.cart}});
+    console.log(updatedUser);
+    if(!updatedUser){
+        res.status(404).send("User Not Found");
+    }
+    res.status(200).send(updatedUser);
+})
+
 module.exports = router;
